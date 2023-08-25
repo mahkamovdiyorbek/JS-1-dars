a9b01a2cc82d17a409d1811644f8449be282edad
mahkamovdiyorbek <mahkamovdiyorbek954@gmail.com>
2023-08-25 17:52:43 +0500

    JS 1-dars
    26-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,95 @@
+/*console.log(13+13+13+13);*/
+
+/*alert("Saytimizga xush kelibsiz")*/
+
+/*document.write("Men body oynasida korinaman")*/
+
+/*let x = 2;
+let y = 5;
+x = 10;*/
+/*let x = 2, y = 5, z = 12;*/
+/*let salom = "Assalomu alekum"*/
+/*let rang = "#f1f1f1"*/
+/*document.write(rang)*/
+
+/* Data types in JS */
+
+/*let son = 45; */                   /* Number */
+/*let matn = "Dasturlash" */            /* String */
+
+/*document.write(typeof matn)*/
+
+
+/* Math in JS*/
+/*let x = 5;*/
+/*x+=5;*/  /* x = 5 + 5 */
+/*x-=5;*/  /* x = 5 - 5 */
+/*x*=5*/   /* x = 5 * 5 */
+/*x/=5*/   /* x = 5 / 5 */
+/*let qoldiq = x%3*/
+/*let natija = x**5;*/    /* 5*5*5*5*5 */
+/*--x;*/
+/*++x*/
+/*x--*/
+/*x++*/
+/*document.write(x+"matn")*/
+
+/* if (agar) */
+/* else (u holoda) */
+/* else if ( yoki bo'lmasa ) => shart formulalari */
+
+/*if (45 == 44){
+    console.log("Ular teng!");
+}
+else {
+    console.log("Ular teng emas!")
+}*/
+
+/*if ( 25==16 ){
+    console.log("tenglik to'g'ri")
+}
+else{
+    console.log("tenglik noto'g'ri")
+}
+
+if ("salom"=="hayr"){
+    console.log("ular bir hil!")
+}
+else {
+    console.log("ular har hil!")
+}*/
+
+/*
+if (45==44){
+    console.log("Qiymatlar teng! -1")
+}
+else if (45==45){
+    console.log("Qiymatlar teng! -2")
+}
+else {
+    console.log("Birorta qiymat teng emas!")
+}
+*/
+
+let a = 10;
+let b = 40;
+let c = 46;
+let d = 50;
+
+/*if (a==10 && b==40 && c==46 && d==50){
+    console.log("teng")
+}
+else {
+    console.log("teng emas")
+}*/
+if (a==10 || b==40 || c==46 || d==50){
+    console.log("teng")
+}
+else {
+    console.log("teng emas")
+}
+
+
+
+
+
